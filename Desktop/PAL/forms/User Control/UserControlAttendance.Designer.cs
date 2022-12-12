caaf91fdca70fb5acadbd4efd79aa477a2d5c174
mohib <mohibrehman31@gmail.com>
2022-12-12 16:17:12 +0500

    m

---
+++ Desktop/PAL/forms/User Control/UserControlAttendance.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Attendance_System.PAL.forms.User_Control
+{
+    partial class UserControlAttendance
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPageMarkAttendance = new System.Windows.Forms.TabPage();
+            this.dataGridViewMarkAttendance = new System.Windows.Forms.DataGridView();
+            this.comboBoxClass = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePickerDate = new System.Windows.Forms.DateTimePicker();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPageMarkAttendance.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMarkAttendance)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPageMarkAttendance
+            // 
+            this.tabPageMarkAttendance.Controls.Add(this.dataGridViewMarkAttendance);
+            this.tabPageMarkAttendance.Controls.Add(this.comboBoxClass);
+            this.tabPageMarkAttendance.Controls.Add(this.label3);
+            this.tabPageMarkAttendance.Controls.Add(this.panel7);
+            this.tabPageMarkAttendance.Controls.Add(this.panel6);
+            this.tabPageMarkAttendance.Controls.Add(this.label2);
+            this.tabPageMarkAttendance.Controls.Add(this.label1);
+            this.tabPageMarkAttendance.Controls.Add(this.dateTimePickerDate);
+            this.tabPageMarkAttendance.Location = new System.Drawing.Point(4, 4);
+            this.tabPageMarkAttendance.Name = "tabPageMarkAttendance";
+            this.tabPageMarkAttendance.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMarkAttendance.Size = new System.Drawing.Size(934, 471);
+            this.tabPageMarkAttendance.TabIndex = 0;
+            this.tabPageMarkAttendance.Text = "Mark Attendance";
+            this.tabPageMarkAttendance.UseVisualStyleBackColor = true;
+            this.tabPageMarkAttendance.Leave += new System.EventHandler(this.tabPageMarkAttendance_Leave);
+            // 
+            // dataGridViewMarkAttendance
+            // 
+            this.dataGridViewMarkAttendance.AllowUserToAddRows = false;
+            this.dataGridViewMarkAttendance.AllowUserToDeleteRows = false;
+            this.dataGridViewMarkAttendance.AllowUserToResizeColumns = false;
+            this.dataGridViewMarkAttendance.AllowUserToResizeRows = false;
+            this.dataGridViewMarkAttendance.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewMarkAttendance.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewMarkAttendance.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewMarkAttendance.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewMarkAttendance.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridViewMarkAttendance.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMarkAttendance.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column5,
+            this.Column3,
+            this.Column4});
+            this.dataGridViewMarkAttendance.Location = new System.Drawing.Point(31, 131);
+            this.dataGridViewMarkAttendance.Name = "dataGridViewMarkAttendance";
+            this.dataGridViewMarkAttendance.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridViewMarkAttendance.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridViewMarkAttendance.ShowCellErrors = false;
+            this.dataGridViewMarkAttendance.ShowEditingIcon = false;
+            this.dataGridViewMarkAttendance.ShowRowErrors = false;
+            this.dataGridViewMarkAttendance.Size = new System.Drawing.Size(872, 318);
+            this.dataGridViewMarkAttendance.TabIndex = 0;
+            this.dataGridViewMarkAttendance.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridViewMarkAttendance_CellFormatting);
+            // 
+            // comboBoxClass
+            // 
+            this.comboBoxClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxClass.FormattingEnabled = true;
+            this.comboBoxClass.Location = new System.Drawing.Point(471, 71);
+            this.comboBoxClass.Name = "comboBoxClass";
+            this.comboBoxClass.Size = new System.Drawing.Size(222, 25);
+            this.comboBoxClass.TabIndex = 2;
+            this.comboBoxClass.SelectedIndexChanged += new System.EventHandler(this.comboBoxClass_SelectedIndexChanged);
+            this.comboBoxClass.Click += new System.EventHandler(this.comboBoxCourse_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(468, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Class:";
+            // 
+            // panel7
+            // 
+            this.panel7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel7.BackColor = System.Drawing.Color.LightGray;
+            this.panel7.Location = new System.Drawing.Point(120, 99);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(270, 2);
+            this.panel7.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel6.BackColor = System.Drawing.Color.Transparent;
+            this.panel6.Location = new System.Drawing.Point(347, 73);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(10, 23);
+            this.panel6.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(117, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Date:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(31)))), ((int)(((byte)(125)))));
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mark Attendance:";
+            // 
+            // dateTimePickerDate
+            // 
+            this.dateTimePickerDate.CustomFormat = "yyyy/MM/dd";
+            this.dateTimePickerDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerDate.Location = new System.Drawing.Point(120, 73);
+            this.dateTimePickerDate.Name = "dateTimePickerDate";
+            this.dateTimePickerDate.Size = new System.Drawing.Size(270, 23);
+            this.dateTimePickerDate.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tabControl1.Controls.Add(this.tabPageMarkAttendance);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(942, 501);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Student_ID";
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Student_Name";
+            this.Column2.HeaderText = "Student Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "Student_Reg";
+            this.Column5.HeaderText = "Reg No.";
+            this.Column5.Name = "Column5";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Status";
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "Attendance_Status";
+            this.Column4.HeaderText = "Status";
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // UserControlAttendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "UserControlAttendance";
+            this.Size = new System.Drawing.Size(942, 501);
+            this.Load += new System.EventHandler(this.UserControlAttendance_Load);
+            this.tabPageMarkAttendance.ResumeLayout(false);
+            this.tabPageMarkAttendance.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMarkAttendance)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage tabPageMarkAttendance;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.ComboBox comboBoxClass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDate;
+        private System.Windows.Forms.DataGridView dataGridViewMarkAttendance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
---
+++ Desktop/PAL/forms/User Control/UserControlAttendance.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance_System.PAL.forms.User_Control
+{
+    public partial class UserControlAttendance : UserControl
+    {
+
+        private string sql = @"Data Source= .\SQLEXPRESS;
+                    Initial Catalog= Attendance_Management_System;
+                    Integrated Security = True;";
+
+        private bool okay;
+        public UserControlAttendance()
+        {
+            InitializeComponent();
+            dataGridViewMarkAttendance.Columns["Column1"].Visible = false;
+            dataGridViewMarkAttendance.Columns["Column5"].Visible= false;
+        }
+
+        private void UserControlAttendance_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCourse_Click(object sender, EventArgs e)
+        {
+            comboBoxClass.Items.Clear();
+            Attendance.Attendance.FillComboBox("SELECT DISTINCT(Class_Name) from Class_Table;", comboBoxClass, sql);
+        }
+
+        private void comboBoxClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Attendance.Attendance.IsMarkAttendance(dateTimePickerDate.Text, comboBoxClass.SelectedItem.ToString(), sql))
+            {
+                Attendance.Attendance.DisplayAndSearchAllData("SELECT Student_Table.Student_ID,Student_Name,Attendance_Status FROM Student_Table INNER JOIN Attendance_Table ON Student_Table.Student_ID = Attendance_Table.Student_ID INNER JOIN Class_Table ON Class_Table.Class_ID = Student_Table.Class_ID WHERE Attendance_Date = '" + dateTimePickerDate.Text + "' AND Class_Name = '" + comboBoxClass.SelectedItem.ToString() + "';", dataGridViewMarkAttendance,sql);
+                okay = true;
+            }
+            else
+            {
+                Attendance.Attendance.DisplayAndSearchAllData("SELECT Student_ID,Student_Name FROM Student_Table INNER JOIN Class_Table ON Class_Table.Class_ID = Student_Table.Class_ID WHERE Class_Name = '" + comboBoxClass.SelectedItem.ToString() + "';", dataGridViewMarkAttendance, sql);
+                okay = false;
+            }
+        }
+
+        private void tabPageMarkAttendance_Leave(object sender, EventArgs e)
+        {
+            if(comboBoxClass.SelectedIndex != -1)
+            {
+                string status;
+                if (Attendance.Attendance.IsMarkAttendance(dateTimePickerDate.Text, comboBoxClass.SelectedItem.ToString(), sql))
+                {
+                    foreach (DataGridViewRow row in dataGridViewMarkAttendance.Rows)
+                    {
+                        if (Convert.ToBoolean(row.Cells["Column4"].EditedFormattedValue)==true)
+                        {
+                            status = "Present";
+                        }
+                        else
+                        {
+                            status = "Absent";
+                        }
+                        Attendance.Attendance.UpdateAttendance(row.Cells["Column1"].Value.ToString(), dateTimePickerDate.Text, status, sql);
+                    }
+                }
+
+                else
+                {
+                    foreach (DataGridViewRow row in dataGridViewMarkAttendance.Rows)
+                    {
+                        if (Convert.ToBoolean(row.Cells["Column4"].EditedFormattedValue) == true)
+                        {
+                            status = "Present";
+                        }
+                        else
+                        {
+                            status = "Absent";
+                        }
+                        Attendance.Attendance.UpdateAttendance(row.Cells["Column1"].Value.ToString(), dateTimePickerDate.Text, status, sql);
+                    }
+                }
+
+            }
+        }
+
+        private void dataGridViewMarkAttendance_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (comboBoxClass.SelectedIndex != -1)
+            {
+                if (Attendance.Attendance.IsMarkAttendance(dateTimePickerDate.Text, comboBoxClass.SelectedItem.ToString(), sql) && okay)
+                {
+                    foreach (DataGridViewRow row in dataGridViewMarkAttendance.Rows)
+                    {
+                        if (row.Cells["Column5"].Value.ToString() == "Present")
+                            row.Cells["Column4"].Value = true;
+                        else
+                            row.Cells["Column4"].Value = false;
+                    }
+                }
+
+            }
+        }
+    }
+}
